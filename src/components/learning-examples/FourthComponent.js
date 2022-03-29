808ac8d6b0236b0d5b1577216717f6dc5ded75b9
quedacoder <quedacoder@gmail.com>
2022-03-28 20:38:10 -0500

    learning how to create components

---
+++ src/components/learning-examples/FourthComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function FourthComponent() {
+  return (
+      <div className="FourthComponent">
+        FourthComponent
+    </div>
+    );
+}
+
+export default FourthComponent;
---
+++ src/components/learning-examples/ThirdComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function ThirdComponent() {
+  return (
+      <div className="ThirdComponent">
+        ThirdComponent
+    </div>
+  );
+}
+
+export default ThirdComponent;
